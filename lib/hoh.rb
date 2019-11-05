d2410001313d31a8aa81ea3ede5e3fdab0956b4d
Joanna E K <kalemajoanna@gmail.com>
2019-11-05 13:00:33 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -38,6 +38,12 @@ def naming_system
        label: "Class",
        sub_category: {
          label: "Order"
+         sub_category: {
+           label: "Family"
+           sub_category: {
+             label: "Genus"
+            }
+           }
           }
          }
        }
